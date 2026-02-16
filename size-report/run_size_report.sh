8cda415533a51db436ebbf84293e7aa0c2f0bd52
sandra_zivkovic <sandra.zivkovic@microblink.com>
2026-02-16 11:04:52 +0100

    [v3000.0.0] delete extra files

--- size-report/run_size_report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd -P`
-popd > /dev/null
-
-$SCRIPTPATH/script/size_report.sh "BlinkCard" $SCRIPTPATH/../BlinkCardSample BlinkCard-SimpleIntegration $SCRIPTPATH/sdk_size_report.md "armeabi-v7a arm64-v8a"
